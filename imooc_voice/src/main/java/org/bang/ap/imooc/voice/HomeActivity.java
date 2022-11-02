c596949ac901e96bd5c890bb01b4bda0fc694ee9
pujian <pujian.pj@foxmail.com>
2022-11-02 15:12:19 +0800

    Initial commit

---
+++ imooc_voice/src/main/java/org/bang/ap/imooc/voice/HomeActivity.java
@@ -0,0 +1,24 @@
+package org.bang.ap.imooc.voice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+public class HomeActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initData() {
+
+    }
+}
